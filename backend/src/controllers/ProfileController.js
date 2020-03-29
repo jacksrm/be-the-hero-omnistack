8053143eeffc7ef5bd421494a49db3070220d9d7
Jacosn Rodrigues <62717829+jacksrm@users.noreply.github.com>
2020-03-29 19:38:31 -0300

    Frontend do app

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,16 +3,11 @@ const connection  = require('../database/connection');
 module.exports = {
     async index(request, response) {
         const ong_id = request.headers.authorization;
-        const { page = 1 } = request.query;
-        const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
             .where('ong_id', ong_id)
-            .limit(5)
-            .offset((page - 1) * 5)
-            .select('*')
+            .select('*');
 
-        response.header('X-Total-Count', count['count(*)']);
         return response.json(incidents);
     }
 };
---
+++ frontend/src/router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profiles from './pages/Profiles';
+import NewIncident from './pages/NewIncident';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+                <Route path="/profiles" component={Profiles} />
+                <Route path="/incidents/new" component={NewIncident} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
